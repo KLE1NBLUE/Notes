648ad61c504215311940133fb8eba19a6a5d8de3
KLE1NBLUE <1049834396@qq.com>
2020-11-19 14:23:25 +0800

    update

---
+++ Junit/src/Calculator2.java
@@ -0,0 +1,21 @@
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class Calculator2 {
+    @Before
+    public void before() {
+        System.out.println("测试前的准备工作，比如链接数据库等等");
+    }
+    @After
+    public void after() {
+        System.out.println("测试结束后的工作，比如关闭链接等等");
+    }
+
+    @Test
+    public void testSum1() {
+        int result = SumUtil.sum1(1, 2);
+        Assert.assertEquals(result, 3);
+    }
+}
